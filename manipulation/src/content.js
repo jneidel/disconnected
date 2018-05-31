223bc6a91d768d06f277960ed21229e66825f2d7
jneidel <jneidel@outlook.com>
2018-05-31 10:46:56 +0200

    Change manipulation content

--- manipulation/src/content.js
+++ manipulation/src/content.js
@@ -1,21 +1,20 @@
 const range = require( "py-range" );
 
 const qs = [
-  { q: "Leckerste Frucht", a: { v: "Apfel", l: 524 }, b: { v: "Apfel rot", l: 64 }, c: { v: "Banane", l: 208 } },
-  { q: "Schönste Stadt", a: { v: "Paris", l: 315 }, b: { v: "Dubei", l: 880 }, c: { v: "Tokyo", l: 1495 } },
-  { q: "Bestes Foto", a: { v: "Sonnenblume", l: 430 }, b: { v: "Gänseblümchen", l: 687 }, c: { v: "Gerbara", l: 123 } },
-  { q: "Was ist deine Lieblings Social Media Plattform", a: { v: "Facebook", l: 976 }, b: { v: "Snapchat", l: 129 }, c: { v: "Instagram", l: 356 } },
-  { q: "Seit wann gibt es Google", a: { v: "1994", l: 329 }, b: { v: "1998", l: 119 }, c: { v: "2001", l: 82 }, correct: "b" },
-  { q: "Was enthält am meisten Eisen", a: { v: "Spinat", l: 832 }, b: { v: "Schokolade", l: 40 }, c: { v: "Rindfleisch", l: 1345 }, correct: "b" },
+  { q: "Bester Apfel", a: { v: "Schöner Apfel", l: 10 }, b: { v: "Mittler Apfel", l: 97 }, c: { v: "Hässlicher Apfel", l: 300 } },
+  { q: "Schönste Stadt", a: { v: "Paris", l: 2 }, b: { v: "Dubei", l: 10 }, c: { v: "Tokyo", l: 80 } },
+  { q: "Bestes Foto", a: { v: "Sonnenblume", l: 230 }, b: { v: "Gänseblümchen", l: 687 }, c: { v: "Gerbara", l: 75 } },
+  { q: "Was ist deine Lieblings Social Media Plattform", c: { v: "Facebook", l: 976 }, b: { v: "Snapchat", l: 129 }, a: { v: "Instagram", l: 356 } },
+  { q: "Seit wann gibt es Google", c: { v: "1994", l: 329 }, a: { v: "1998", l: 119 }, b: { v: "2001", l: 82 }, correct: "a" },
+  { q: "Was enthält am meisten Eisen", a: { v: "Spinat", l: 832 }, b: { v: "Schokolade", l: 50 }, c: { v: "Rindfleisch", l: 1345 }, correct: "b" },
   { q: "Was schmeckt besser", a: { v: "Pepsi", l: 423 }, b: { v: "Coca Cola", l: 124 } },
   { q: "Wie viele Mitglieder hat Facebook", a: { v: "2 mrd", l: 100 }, b: { v: "200 mio", l: 31 }, c: { v: "1,3 mrd", l: 421 }, correct: "a" },
-  { q: "Welchen Beruf lernte Stefan Raab ursprünglich", a: { v: "Tischler", l: 5698 }, b: { v: "Erzieher", l: 2243 }, c: { v: "Metzger", l: 430 }, correct: "c" },
-  { q: "Welche Stadt der Welt hat die meisten Einwohner", a: { v: "Peking", l: 708 }, b: { v: "New York", l: 1544 }, c: { v: "Tokyo", l: 4456 } },
+  { q: "Welchen Beruf lernte Stefan Raab ursprünglich", c: { v: "Tischler", l: 758 }, a: { v: "Erzieher", l: 450 }, b: { v: "Metzger", l: 265 }, correct: "b" },
+  { q: "Welche Stadt der Welt hat die meisten Einwohner", a: { v: "Peking", l: 446 }, b: { v: "New York", l: 692 }, c: { v: "Tokyo", l: 1234 } },
 ];
 
 /* ( function generatePug() {
   const out = qs.map( x => ( { qtext: `${x.q}?`, q: `head${qs.indexOf( x )}`, b1: `bar${qs.indexOf( x )}a`, b2: `bar${qs.indexOf( x )}b`, options: `a: ${qs[qs.indexOf( x )].a.v}; b: ${qs[qs.indexOf( x )].b.v};${qs[qs.indexOf( x )].c ? ` c: ${qs[qs.indexOf( x )].c.v}` : ""}` } ) );
-  //  options: `a: ${qs[qs.indexOf( x )].a.v}; b: ${qs[qs.indexOf( x )].b.v};${qs[qs.indexOf( x )].c.v ? ` c: ${qs[qs.indexOf( x )].c.v}` : ""}`
 
   console.log( JSON.stringify( out ) );
 } )(); */
